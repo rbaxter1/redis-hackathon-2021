1f61172f7b0ec6732f3baa7b9a2edd28ea8e396c
Jonas <jonasmmartin@gmail.com>
2021-05-10 23:09:15 -0400

    removed glamorous-native package to get web rendering working again

--- mobile/src/ItemImage.js
+++ mobile/src/ItemImage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
-import glamorous from 'glamorous-native';
 
 const styles = StyleSheet.create({
     container: {
@@ -16,36 +15,11 @@ const styles = StyleSheet.create({
     }
 });
 
-const ImageContainer = glamorous.view((props, theme) => ({
-    flex: 1,
-    alignItems: 'stretch'
-}));
-
-const StyledImage = glamorous.image((props, theme) => ({
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0
-}));
-
-// function ImageContainer () {
-//     return (
-//         <View style={styles.container}></View>
-//     );
-// }
-
-// function TheImage () {
-//     return (
-//         <Image style={styles.image}></Image>
-//     );
-// }
-
 const ItemImage = props => {
     return (
-        <ImageContainer>
-            <StyledImage resizeMode="contain" {...props} />
-        </ImageContainer>
+        <View style={styles.container}>
+            <Image style={styles.image} resizeMode="contain" {...props} />
+        </View>
     );
 }
 
--- mobile/src/PreviewCard.js
+++ mobile/src/PreviewCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import glamorous from 'glamorous-native';
 
 import ItemImage from './ItemImage'
 
 const styles = StyleSheet.create({
-    container: {
+    cardContainer: {
         height: 160,
         width: '85%',
         left: '7.5%',
@@ -17,73 +16,30 @@ const styles = StyleSheet.create({
     },
     nameContainer: {
         height: '30%',
+        backgroundColor: '#3f51b5',
         justifyContent: 'center'
     },
     title: {
-        fontFamily: 'robotoRegular',
         fontSize: 16,
+        color: '#000000',
         lineHeight: 24,
         textAlign: 'left',
         alignSelf: 'center'
     }
 });
 
-const Title = glamorous.text((props, theme) => ({
-    fontSize: 16,
-    color: props.color || '#000000',
-    lineHeight: 24,
-    textAlign: props.align || 'left',
-    alignSelf: props.alignSelf || 'center'
-}));
-
-const CardContainer = glamorous.view((props, theme) => ({
-    height: 160,
-    width: '85%',
-    left: '7.5%',
-    justifyContent: 'space-around'
-}));
-
-const ImageContainer = glamorous.view((props, theme) => ({
-    flex: 1,
-    alignItems: 'stretch'
-}));
-
-const NameContainer = glamorous.view((props, theme) => ({
-    height: '30%',
-    backgroundColor: '#3f51b5',
-    justifyContent: 'center'
-}));
-
-// function CardContainer () {
-//     return (
-//         <View style={styles.container}></View>
-//     );
-// }
-
-// function ImageContainer () {
-//     return (
-//         <View style={styles.imageContainer}></View>
-//     );
-// }
-
-// function NameContainer () {
-//     return (
-//         <View style={styles.nameContainer}></View>
-//     );
-// }
-
 const PreviewCard = ({name, imageUrl}) => {
     return (
-        <CardContainer>
-            <ImageContainer>
+        <View style={styles.cardContainer}>
+            <View style={styles.imageContainer}>
                 <ItemImage source={{uri: imageUrl}} />
-            </ImageContainer>
-            <NameContainer>
-                <Title align="center">
+            </View>
+            <View style={styles.nameContainer}>
+                <Text style={styles.title} align="center">
                     {name}
-                </Title>
-            </NameContainer>
-        </CardContainer>
+                </Text>
+            </View>
+        </View>
     );
 };
 
