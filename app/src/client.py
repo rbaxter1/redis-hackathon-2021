08c81eeeeb667f61e2a344179def3545b910635b
Slattery Donohoe <slattery.donohoe@gmail.com>
2021-05-15 12:27:40 -0400

    Added LoadTestData command to client.py, which pre-loads the DB with users and networks. Item additions are TODO

--- app/src/client.py
+++ app/src/client.py
@@ -2,6 +2,25 @@ from google.protobuf.json_format import MessageToJson, Parse
 import grpc
 import network_pb2
 import network_pb2_grpc
+import json
+import random
+
+
+def CreateNetwork(stub, newNetwork):
+    return stub.CreateNetwork(
+        network_pb2.CreateNetworkRequest(network=newNetwork))
+
+
+def GetNetworksForUser(stub, userEmail):
+    return stub.GetNetworksForUser(
+        network_pb2.GetNetworksForUserRequest(email=userEmail))
+
+
+def JoinNetwork(stub, userEmail, networkName):
+    return stub.JoinNetwork(
+        network_pb2.JoinNetworkRequest(
+            email=userEmail, network_name=networkName)
+    )
 
 
 def run(cmd):
@@ -14,29 +33,63 @@ def run(cmd):
         if cmd == "CreateNetwork":
             newNetwork = network_pb2.NetworkDetails()
             newNetwork.name = 'myNetwork1218'
-            newNetwork.owner_id = 'rbaxter@ezesoft.com'
+            newNetwork.owner_id = 'jsmith@gmail.com'
             newNetwork.description = 'some blah description pt 5'
             image_string = "image_data_12345"
             newNetwork.image = image_string.encode('utf-8')
-            print(newNetwork.image)
-            response = stub.CreateNetwork(
-                network_pb2.CreateNetworkRequest(network=newNetwork))
+            response = CreateNetwork(stub, newNetwork)
             print("Greeter client received: " + response.network_name)
+
         # GetNetworksForUser endpoint
         elif cmd == "GetNetworksForUser":
-            userEmail = 'rbaxter@ezesoft.com'
-            response = stub.GetNetworksForUser(
-                network_pb2.GetNetworksForUserRequest(email=userEmail))
+            response = GetNetworksForUser(stub, 'jsmith@gmail.com')
             print(MessageToJson(response))
+
         # JoinNetwork endpoint
         elif cmd == "JoinNetwork":
-            userEmail = 'slattery.donohoe@gmail.com'
-            networkName = 'myNetwork5'
-            response = stub.JoinNetwork(
-                network_pb2.JoinNetworkRequest(
-                    email=userEmail, network_name=networkName)
-            )
+            JoinNetwork(stub, 'mdavis@gmail.com', 'myNetwork1109')
 
+        # Command to populate DB with dummy data
+        elif cmd == "LoadTestData":
+            print("Loading test data...")
+            data = ''
+            with open('./client_data/client_data.json') as f:
+                data = json.load(f)
+            # create all users
+            print("Creating %d users" % len(data['users']))
+            for u in data['users']:
+                userDetails = Parse(json.dumps(u), network_pb2.UserDetails())
+                stub.CreateUser(
+                    network_pb2.CreateUserRequest(user=userDetails))
+            # create all networks
+            print("Creating %d networks" % len(data['networks']))
+            for n in data['networks']:
+                users = []
+                alreadyOwned = 'owner_id' in n
+                # If an owner's been assigned, get 9 other users
+                if alreadyOwned:
+                    users = random.sample(data['users'], 9)
+                    # Make sure not to re-add the owner
+                    while any(u['email'] == n['owner_id'] for u in users):
+                        users = random.sample(data['users'], 9)
+                # Otherwise just get 10 random users and assign the first one as owner
+                else:
+                    users = random.sample(data['users'], 10)
+                    n['owner_id'] = users[0]['email']
+                    users.pop(0)
+
+                # Create the network itself
+                newNetwork = network_pb2.NetworkDetails()
+                newNetwork.name = n['name']
+                newNetwork.description = n['description']
+                newNetwork.owner_id = n['owner_id']
+                CreateNetwork(stub, newNetwork)
+
+                # Add users to network
+                for u in users:
+                    JoinNetwork(stub, u['email'], n['name'])
+
+        # Test to create user and get user
         elif cmd == "test":
             userDetails = network_pb2.UserDetails()
             userDetails.first_name = "John"
